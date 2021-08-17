3e4d75fb6e186fc13a4ab4037bb506d45a9440a6
Kyle Gerard Felker <felker@anl.gov>
2021-08-17 05:12:35 +0000

    Fix spacing and parentheses

--- bin/frnn_onnx_gs_change_bs.py
+++ bin/frnn_onnx_gs_change_bs.py
@@ -10,21 +10,20 @@ tensors = graph.tensors()
 batch_sizes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
 for bs in batch_sizes:
-	tensors["input_2"].shape[0] = bs
-	tensors["time_distributed"].shape[0] = bs
-	tensors["shape_tensor1"] = gs.Constant(
-		name='shape_tensor1', values=np.array([bs, 128, 1], dtype=np.int32))
-	graph.nodes[-1].inputs[-1] = gs.Constant(
-		name='shape_tensor1', values=np.array([bs, 128, 1], dtype=np.int32)
-	onnx.save(gs.export_onnx(graph.cleanup().toposort()), f"frnn-bs{bs}-seq128.onnx")
+    tensors["input_2"].shape[0] = bs
+    tensors["time_distributed"].shape[0] = bs
+    tensors["shape_tensor1"] = gs.Constant(
+	    name='shape_tensor1', values=np.array([bs, 128, 1], dtype=np.int32))
+    graph.nodes[-1].inputs[-1] = gs.Constant(
+	    name='shape_tensor1', values=np.array([bs, 128, 1], dtype=np.int32))
+    onnx.save(gs.export_onnx(graph.cleanup().toposort()), f"frnn-bs{bs}-seq128.onnx")
 
-# dynamic 
-bs = -1 
+# dynamic
+bs = -1
 tensors["input_2"].shape[0] = gs.Tensor.DYNAMIC
 tensors["time_distributed"].shape[0] = gs.Tensor.DYNAMIC
 tensors["shape_tensor1"] = gs.Constant(
-	name='shape_tensor1', values=np.array([gs.Tensor.DYNAMIC, 128, 1], dtype=np.int32))
+    name='shape_tensor1', values=np.array([gs.Tensor.DYNAMIC, 128, 1], dtype=np.int32))
 graph.nodes[-1].inputs[-1] = gs.Constant(
-	name='shape_tensor1', values=np.array([gs.Tensor.DYNAMIC, 128, 1], dtype=np.int32)
+    name='shape_tensor1', values=np.array([gs.Tensor.DYNAMIC, 128, 1], dtype=np.int32))
 onnx.save(gs.export_onnx(graph.cleanup().toposort()), f"frnn-bs-dynamic-seq128.onnx")
-
